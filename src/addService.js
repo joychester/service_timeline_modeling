7dc48672e100a96c6993b9ad4c0ecef40b234a6a
Cheng Chi (@joychester) <joychester@gmail.com>
2022-11-18 15:08:49 -0800

    Update addService.js

--- src/addService.js
+++ src/addService.js
@@ -1,34 +1,39 @@
-google.charts.load("current", { packages: ["timeline"] });
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-  const container = document.getElementById("timeline");
-  const chart = new google.visualization.Timeline(container);
-  const dataTable = new google.visualization.DataTable();
+      google.charts.load('current', {'packages':['timeline']});
+      google.charts.setOnLoadCallback(drawChart);
+      let currentStart = 0;
+      let currentEnd = 0;
+      
+      function drawChart() {
+        const container = document.getElementById('timeline');
+        const chart = new google.visualization.Timeline(container);
+        const dataTable = new google.visualization.DataTable();
 
-  dataTable.addColumn({ type: "string", id: "API Name" });
-  dataTable.addColumn({ type: "number", id: "Start" });
-  dataTable.addColumn({ type: "number", id: "Duration" });
-  dataTable.addRows([["API-1", 0, 20]]);
-
-  chart.draw(dataTable);
-}
-
-function appendService(
-  dataTable,
-  chart,
-  currentStart,
-  currentEnd,
-  serviceName,
-  loadInParallel,
-  serviceDuration
-) {
-  const serviceDetails = [];
-  serviceDetails.push(serviceName);
-  if (loadInParallel === true) {
-    serviceDetails.push(currentStart);
-    currentEnd = serviceDuration;
-  } else {
-    serviceDetails.push(currentEnd);
-  }
-  dataTable.addColumn([serviceDetails]);
-}
+        dataTable.addColumn({ type: 'string', id: 'API Name' });
+        dataTable.addColumn({ type: 'number', id: 'Start' });
+        dataTable.addColumn({ type: 'number', id: 'End' });
+        
+        appendService(dataTable, chart, 'API-1', false, 20);
+        appendService(dataTable, chart, 'API-2', false, 10);
+        appendService(dataTable, chart, 'API-3', false, 30);
+        
+        chart.draw(dataTable);
+      }
+      
+      function appendService(dataTable, chart, serviceName, loadInParallel, serviceDuration) {
+          const serviceDetails = [];
+          // add service Name
+          serviceDetails.push(serviceName);
+          // add Start Number
+          if (loadInParallel === true) {
+            serviceDetails.push(currentStart);
+            currentEnd = (serviceDuration > (currentEnd - currentStart)) ? (currentStart + serviceDuration) : currentEnd;
+          } else {
+            serviceDetails.push(currentEnd);
+            currentStart = currentEnd;
+            currentEnd = currentStart + serviceDuration;
+          }
+          // add Duration Number
+          serviceDetails.push(currentEnd);
+          console.log(serviceDetails);
+          dataTable.addRows([serviceDetails]);
+      }
