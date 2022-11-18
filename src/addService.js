cbe8119c9df030e9ba3b021d128ea7caa7c7faa5
Cheng Chi (@joychester) <joychester@gmail.com>
2022-11-18 15:46:32 -0800

    Update addService.js

--- src/addService.js
+++ src/addService.js
@@ -3,7 +3,11 @@
       let currentStart = 0;
       let currentEnd = 0;
       
-      function drawChart() {
+      const addServiceButton = document.getElementById("clearScreenBtn");
+			addServiceButton.addEventListener("click", initChart, false);
+      
+      function initChart() {
+        const gchartContext = {};
         const container = document.getElementById('timeline');
         const chart = new google.visualization.Timeline(container);
         const dataTable = new google.visualization.DataTable();
@@ -11,21 +15,33 @@
         dataTable.addColumn({ type: 'string', id: 'API Name' });
         dataTable.addColumn({ type: 'number', id: 'Start' });
         dataTable.addColumn({ type: 'number', id: 'End' });
-            
-        appendService(dataTable, chart, 'API-1', false, 20);
-        appendService(dataTable, chart, 'API-2', false, 10);
-        appendService(dataTable, chart, 'API-3', false, 30);
-        appendService(dataTable, chart, 'API-4', true, 20);
-        appendService(dataTable, chart, 'API-5', false, 10);
         
         chart.draw(dataTable);
+        
+        gchartContext['chart'] = chart;
+        gchartContext['dataTable'] = dataTable;
+        return gchartContext;
+      }
+      
+      function drawChart() {
+        const gchartContext = initChart();
+        /*
+        const addServiceButton = document.getElementById("addServiceBtn");
+				addServiceButton.addEventListener("click", () => { appendService(dataTable, chart, 'API-1', false, 20); }, false);
+        */
+        appendService(gchartContext.dataTable, gchartContext.chart, 'API-1', false, 20);
+        appendService(gchartContext.dataTable, gchartContext.chart, 'API-2', false, 10);
+        appendService(gchartContext.dataTable, gchartContext.chart, 'API-3', false, 30);
+        appendService(gchartContext.dataTable, gchartContext.chart, 'API-4', true, 20);
+        appendService(gchartContext.dataTable, gchartContext.chart, 'API-5', false, 10);
+        gchartContext.chart.draw(gchartContext.dataTable);
       }
       
       function appendService(dataTable, chart, serviceName, loadInParallel, serviceDuration) {
           const serviceDetails = [];
-            
+          // add service Name
           serviceDetails.push(serviceName);
-            
+          // add Start Number
           if (loadInParallel === true) {
             serviceDetails.push(currentStart);
             serviceDetails.push(currentStart + serviceDuration);
@@ -37,6 +53,6 @@
             currentEnd = currentStart + serviceDuration;
           }
           
-          //console.log(serviceDetails);
+          console.log(serviceDetails);
           dataTable.addRows([serviceDetails]);
       }
