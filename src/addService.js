7a16d3e6ec0600da039b997d0591be1b838b72f0
Cheng Chi (@joychester) <joychester@gmail.com>
2022-11-19 00:25:25 -0800

    Update addService.js

--- src/addService.js
+++ src/addService.js
@@ -27,16 +27,16 @@
         clearScreenButton.addEventListener("click", initChart, false);
       
         const addServiceButton = document.getElementById("addServiceBtn");
-        addServiceButton.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-1', false, 10); }, false);
+        addServiceButton.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-A', false, 10); }, false);
         
         const addServiceButton2 = document.getElementById("addServiceBtn2");
-        addServiceButton2.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-2', false, 20); }, false);
+        addServiceButton2.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-B', false, 20); }, false);
         
         const addServiceButton3 = document.getElementById("addServiceBtn3");
-        addServiceButton3.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-3', true, 15); }, false);
+        addServiceButton3.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-C', true, 15); }, false);
         
         const addServiceButton4 = document.getElementById("addServiceBtn4");
-        addServiceButton4.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-4', true, 30); }, false);
+        addServiceButton4.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-D', true, 30); }, false);
       }
       
       function appendService(dataTable, chart, serviceName, loadInParallel, serviceDuration) {
