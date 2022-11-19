6937372023f928fc811fe1a75352d8c20da30d35
Cheng Chi (@joychester) <joychester@gmail.com>
2022-11-19 00:16:56 -0800

    Update addService.js

--- src/addService.js
+++ src/addService.js
@@ -27,7 +27,16 @@
         clearScreenButton.addEventListener("click", initChart, false);
       
         const addServiceButton = document.getElementById("addServiceBtn");
-        addServiceButton.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-1', false, 20); }, false);
+        addServiceButton.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-1', false, 10); }, false);
+        
+        const addServiceButton2 = document.getElementById("addServiceBtn2");
+        addServiceButton2.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-2', false, 20); }, false);
+        
+        const addServiceButton3 = document.getElementById("addServiceBtn3");
+        addServiceButton3.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-3', true, 15); }, false);
+        
+        const addServiceButton4 = document.getElementById("addServiceBtn4");
+        addServiceButton4.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-4', true, 30); }, false);
       }
       
       function appendService(dataTable, chart, serviceName, loadInParallel, serviceDuration) {
@@ -46,7 +55,7 @@
             currentEnd = currentStart + serviceDuration;
           }
           
-          console.log(serviceDetails);
+          //console.log(serviceDetails);
           dataTable.addRows([serviceDetails]);
           chart.draw(dataTable);
       }
