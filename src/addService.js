0d5aa53c0681212a304e7e9e5a50fad31b798647
Cheng Chi (@joychester) <joychester@gmail.com>
2022-11-19 23:37:24 -0800

    Update addService.js

--- src/addService.js
+++ src/addService.js
@@ -1,45 +1,78 @@
-      google.charts.load('current', {'packages':['timeline']});
+      google.charts.load('current', {
+        'packages': ['timeline']
+      });
       google.charts.setOnLoadCallback(drawChart);
       let currentStart = 0;
       let currentEnd = 0;
+      let api_seq = 1;
       let gchartContext = {};
-      
+
       function initChart() {
+        api_seq = 1;
+        if (document.getElementById('hidden-msg').childElementCount > 0) {
+          const msg = document.getElementById('hidden-msg');
+          msg.removeChild(msg.firstElementChild);
+        }
+
         const container = document.getElementById('timeline');
         const chart = new google.visualization.Timeline(container);
         const dataTable = new google.visualization.DataTable();
 
-        dataTable.addColumn({ type: 'string', id: 'API Name' });
-        dataTable.addColumn({ type: 'number', id: 'Start' });
-        dataTable.addColumn({ type: 'number', id: 'End' });
-        
+        dataTable.addColumn({
+          type: 'string',
+          id: 'API Name'
+        });
+        dataTable.addColumn({
+          type: 'number',
+          id: 'Start'
+        });
+        dataTable.addColumn({
+          type: 'number',
+          id: 'End'
+        });
+
         chart.draw(dataTable);
-        
+
         gchartContext['chart'] = chart;
         gchartContext['dataTable'] = dataTable;
         return gchartContext;
       }
-      
+
       function drawChart(serviceName, loadInParallel, serviceDuration) {
         gchartContext = initChart();
-        
+
         const clearScreenButton = document.getElementById("clearScreenBtn");
         clearScreenButton.addEventListener("click", initChart, false);
-      
-        const addServiceButton = document.getElementById("addServiceBtn");
-        addServiceButton.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-A', false, 10); }, false);
-        
-        const addServiceButton2 = document.getElementById("addServiceBtn2");
-        addServiceButton2.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-B', false, 20); }, false);
-        
-        const addServiceButton3 = document.getElementById("addServiceBtn3");
-        addServiceButton3.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-C', true, 15); }, false);
-        
-        const addServiceButton4 = document.getElementById("addServiceBtn4");
-        addServiceButton4.addEventListener("click", () => { appendService(gchartContext.dataTable, gchartContext.chart, 'API-D', true, 30); }, false);
+
+        const addServiceButton = document.getElementById("addSeqServiceBtn");
+        addServiceButton.addEventListener("click", () => {
+          appendService(gchartContext.dataTable, gchartContext.chart, false);
+        }, false);
+
+        const addServiceButton2 = document.getElementById("addParaServiceBtn");
+        addServiceButton2.addEventListener("click", () => {
+          appendService(gchartContext.dataTable, gchartContext.chart, true);
+        }, false);
       }
-      
-      function appendService(dataTable, chart, serviceName, loadInParallel, serviceDuration) {
+
+      function appendService(dataTable, chart, loadInParallel) {
+        if (!(parseInt(document.getElementById('api-duration').value) > 0)) {
+          if (document.getElementById('hidden-msg').childElementCount === 0) {
+            const str = '<p>You must add some positive value to API duration!</p>',
+              div = document.getElementById('hidden-msg');
+
+            div.insertAdjacentHTML('beforeend', str);
+          }
+
+          return;
+        } else {
+          if (document.getElementById('hidden-msg').childElementCount > 0) {
+            const msg = document.getElementById('hidden-msg');
+            msg.removeChild(msg.firstElementChild);
+          }
+
+          const serviceDuration = parseInt(document.getElementById('api-duration').value);
+          const serviceName = `API-${api_seq++}`;
           const serviceDetails = [];
           // add service Name
           serviceDetails.push(serviceName);
@@ -54,8 +87,8 @@
             currentStart = currentEnd;
             currentEnd = currentStart + serviceDuration;
           }
-          
-          //console.log(serviceDetails);
+          console.log(serviceDetails);
           dataTable.addRows([serviceDetails]);
           chart.draw(dataTable);
+        }
       }
